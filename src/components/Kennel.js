9c960849b31f507d0b6821523868b58b35339943
mgjeffries <gib.jeffries@gmail.com>
2020-09-02 12:03:48 -0400

    add initial files

---
+++ src/components/Kennel.js
@@ -0,0 +1,21 @@
+import React from "react"
+import "./Kennel.css"
+import { Animal } from "./animal/Animal"
+
+export const Kennel = () => (
+    <>
+        <h2>Nashville Kennels</h2>
+        <small>Loving care when you're not there.</small>
+        <address>
+            <div>Visit Us at the Nashville North Location</div>
+            <div>500 Puppy Way</div>
+        </address>
+
+        <h2>Animals</h2>
+        <article className="animals">
+          <Animal />
+          <Animal />
+          <Animal />
+        </article>
+    </>
+)
