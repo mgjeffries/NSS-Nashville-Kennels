3293e28d532f3baab051e28e07bfeda90049b57e
mgjeffries <gib.jeffries@gmail.com>
2020-09-10 14:55:18 -0500

    add button to remove employee

--- src/components/Employee/EmployeeDetail.js
+++ src/components/Employee/EmployeeDetail.js
@@ -8,7 +8,7 @@ import "./Employee.css"
 export const EmployeeDetail = (props) => {
     const { animals, getAnimals } = useContext(AnimalContext)
     const { locations, getLocations } = useContext(LocationContext)
-    const { employees, getEmployees } = useContext(EmployeeContext)
+    const { employees, getEmployees, removeEmployee } = useContext(EmployeeContext)
 
     const [animal, setAnimal] = useState({})
     const [employee, setEmployee] = useState({})
@@ -46,6 +46,10 @@ export const EmployeeDetail = (props) => {
                     : `Currently taking care of ${animal.name}`
                 }
             </div>
+            <button onClick={() => {
+                removeEmployee(employee)
+                    .then(props.history.push("/employees"))
+            }}>Remove Employee</button>
         </section>
     )
 }
