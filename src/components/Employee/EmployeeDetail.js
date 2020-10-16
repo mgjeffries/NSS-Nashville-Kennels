9661f9cf04ad2f48c837c7d5ea1b4475ea5bde44
mgjeffries <gib.jeffries@gmail.com>
2020-10-16 13:32:02 -0500

    remove animal id from employees

--- src/components/Employee/EmployeeDetail.js
+++ src/components/Employee/EmployeeDetail.js
@@ -10,20 +10,13 @@ export const EmployeeDetail = (props) => {
     const { locations, getLocations } = useContext(LocationContext)
     const { employees, getEmployees, removeEmployee } = useContext(EmployeeContext)
 
-    const [animal, setAnimal] = useState({})
     const [employee, setEmployee] = useState({})
     const [location, setLocation] = useState({})
 
     useEffect(() => {
         getEmployees()
-            .then(getAnimals)
             .then(getLocations)
     }, [])
-
-    useEffect(() => {
-        const animal = animals.find(a => a.id === employee.animalId) || {}
-        setAnimal(animal)
-    }, [animals])
 
     useEffect(() => {
         const employee = employees.find(e => e.id === parseInt(props.match.params.employeeId)) || {}
@@ -39,13 +32,6 @@ export const EmployeeDetail = (props) => {
         <section className="employee">
             <h3 className="employee__name">{employee.name}</h3>
             <div>Currently working at { location.name }</div>
-            <div>
-                {
-                (employee.animalId === null)
-                    ? "Not assigned to an animal"
-                    : `Currently taking care of ${animal.name}`
-                }
-            </div>
             <button onClick={() => {
                 removeEmployee(employee)
                     .then(props.history.push("/employees"))
